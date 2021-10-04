8ca45a0b0a2e04d776f86a38006121cc2f160f9f
marufmarzuq <marufmarzuq18@gmail.com>
2021-10-04 19:49:52 +0600

    first checking done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
                               <img src={img} alt="" />
                          </Link>
                          <div className="menu">
+                              <Link to="/">Home</Link>
                               <Link to="/courses">All Courses</Link>
                               <Link to="/about">About</Link>
                               <Link className="sign-in" to="/sign-in">Sign In</Link>
